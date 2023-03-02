19e7b5dab03fc9df15b16b515e204fc880340885
Paul Simroth <paul.simroth@proton.me>
2023-03-02 18:29:21 +0100

    content

--- App.js
+++ App.js
@@ -4,8 +4,20 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{color: "white"}}>Hello World</Text>
-      <Image source={{uri: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"}} style={{height: 25, width: 25}}/>
+      <View style={styles.coinContainer}>
+        <Image 
+          source={{uri: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"}}
+          style={{height: 30, width: 30, marginRight: 10, alignSelf: "center"}}
+        />
+        <View>
+          <Text style={styles.title}>Bitcoin</Text>
+          <View style={{flexDirection: "row"}}>
+            <Text style={styles.text}>1</Text>
+            <Text style={styles.text}>BTC</Text>
+            <Text style={styles.text}>0,63%</Text>
+          </View>
+        </View>
+      </View>
       <StatusBar style="light" />
     </View>
   );
@@ -17,4 +29,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#121212',
     paddingTop: 50,
   },
+  title: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  text: {
+    color: "white",
+
+  },
+  coinContainer: {
+    flexDirection: "row",
+    borderBottomWidth: 0.5,
+    borderBottomColor: "grey",
+    padding: 15
+  }
 });
