6f6f771f245983def12dd5629acd4f38ac395801
Paul Simroth <paul.simroth@proton.me>
2023-03-04 15:25:55 +0100

    cleaning

--- App.js
+++ App.js
@@ -18,30 +18,4 @@ const styles = StyleSheet.create({
     backgroundColor: '#121212',
     paddingTop: 50,
   },
-  title: {
-    color: "white",
-    fontSize: 16,
-    fontWeight: "bold",
-    marginBottom: 3,
-  },
-  text: {
-    color: "white",
-    marginRight: 5,
-  },
-  coinContainer: {
-    flexDirection: "row",
-    borderBottomWidth: 0.5,
-    borderBottomColor: "grey",
-    padding: 15,
-  },
-  rank: {
-    fontWeight: "bold",
-    color: "white",
-  },
-  rankContainer: {
-    backgroundColor: "#585858",
-    paddingHorizontal: 5,
-    borderRadius: 5,
-    marginRight: 5,
-  },
 });
