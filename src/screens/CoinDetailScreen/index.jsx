7da519ae7bf7f926cb8ff897c58b9303293a8b88
Paul Simroth <paul.simroth@proton.me>
2023-03-06 10:15:37 +0100

    CoinDetailScreen added

---
+++ src/screens/CoinDetailScreen/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const CoinDetailScreen = () => {
+    return (
+        <View style={{color: "white"}}>CoinDetailScreen</View>
+    );
+};
+
+export default CoinDetailScreen;
